0ad35243985475dc5dae429d83d1091f7aa7b824
Ritesh Singh <iamriteshsingh03@gmail.com>
2026-06-02 19:59:04 +0530

    Made basic core functionality

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ Better version of my previous hardcoded Read Mode project.<p>
 <p>Starting it...<br>
 Added Basic file structure and added working basic starting flask implementation<br>
 Made Simple form structure that passes the url to the backend.<p>
+
+<p>The main thing starts now<br>
+How to build the actual generalised algorithm<br>
+I have decided a plan to go with<p>
+
+<p>Firstly, Remove any clutter like ad divisions, sidebars, topbars, index etc.<br>
+Removing clutter through keywords is a neccessary risk<br>
+I need to find the balance so that it removes the clutter but not the main content<br>
+So I observed some websites and found some divisions and keywords for obvious clutter<br><br>
+Along with obvious tags like script and style, I will remove some clutter through keyword searching<br>
+keywords like sidebar, feedback, subheader etc.<br>
+<p>
+
+<p>
+Now, next step is finding candidates i.e. the containers or div that hold actual important content<br>
+Now, comes the scoring part<br>
+this is where I am gonna decide how much important the section is<br>
+<p>
+
+<p>
+Now that I have made the basic core functionality,<br>
+I have to primarily debug it and improve it by testing on various article websites<br>
+then<br>
+I have to format it and make it look better.<br>
+<p>
--- app.py
+++ app.py
@@ -2,6 +2,93 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 
+def removeTags(soup):
+    Tags = [
+        "script",
+        "style",
+        "nav",
+        "footer",
+        "header",
+        "aside",
+        "noscript",
+    ]
+
+    for tag in list(soup.find_all(Tags)):
+        tag.decompose()
+
+def removeKeywords(soup):
+    Keywords = [
+        "navigation",
+        "menu",
+        "appearance",
+        "feedback",
+        "footer",
+        "subheader",
+        "cert",
+        "sidebar",
+        "ads",
+        "popups",
+        "related",
+        "share",
+        "comment",
+    ]
+
+    for tag in list(soup.find_all()):
+        if tag.attrs is None:
+            continue
+
+        classes = tag.attrs.get("class")
+
+        if not classes:
+            continue
+
+        class_string = " ".join(classes).lower()
+
+        for keyword in Keywords:
+
+            if keyword in class_string:
+                tag.decompose()
+                print(f"Removed: {class_string}")
+                break
+
+def findCandidates(soup):
+
+    candidates = soup.find_all(["article", "main", "section", "div"])
+    return candidates
+
+def scoreCandidates(candidate):
+    text = candidate.get_text(strip = True)
+    textLength = len(text)
+
+    para = candidate.find_all("p")
+    paraCount = len(para)
+
+    links = candidate.find_all("a")
+    linkCount = len(links)
+
+    score = ( textLength + paraCount*200 - linkCount*50)
+    return score
+
+def bestCandidate(candidates):
+    bestSection = None
+    bestScore = 0
+
+    for candidate in candidates:
+        score = scoreCandidates(candidate)
+
+        print(score)
+        print(candidate.name)
+        print(candidate.get_text(strip = True)[:100])
+        print("-" * 50)
+
+        if score > bestScore:
+            bestScore = score
+            bestSection = candidate
+
+    return bestSection
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,7 +100,21 @@ def extract():
     article_url = request.form.get("url")
     response = requests.get(article_url)
     soup = BeautifulSoup(response.text, "html.parser")
-    return render_template("read.html", website = soup.title.string)
+
+    removeTags(soup)
+    removeKeywords(soup)
+
+    sections = findCandidates(soup)
+    
+    best = bestCandidate(sections)
+
+    if best:
+        content = best.get_text()
+    else:
+        content = "No readable content found"
+
+
+    return render_template("read.html", website = soup.title.string, article_text = content)
 
 if __name__ == "__main__":
     app.run(debug = True)
